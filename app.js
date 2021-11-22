22543658d0b26c5eb5499d99dcb64fbb0239dbec
Conrado Guerra <conradoguerra@gmail.com>
2021-11-22 08:31:14 -0300

    Creating an user and validating with graphql

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const feedRoutes = require("./routes/feed");
-const authRoutes = require("./routes/auth");
 //Importing mongoose to create a model
 const mongoose = require("mongoose");
-const multer = require('multer')
-const path = require('path')
+const multer = require("multer");
+const path = require("path");
+const { graphqlHTTP } = require("express-graphql");
+const graphqlSchema = require("./graphql/schema");
+const graphqlResolver = require("./graphql/resolvers");
 
 const app = express();
 
@@ -13,30 +14,36 @@ const fileStorage = multer.diskStorage({
   //Setting the destination of the file
   destination: (req, file, cb) => {
     //(if error, folder)
-   cb(null, 'images')
+    cb(null, "images");
   },
   //Setting the filename of the file
   filename: (req, file, cb) => {
     //(if error, name)
-    cb(null, Date.now() + '-' +file.originalname)
-  }
-})
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
 
 //Creating a filter of files
 const fileFilter = (req, file, cb) => {
-  if(file.mimetype === 'image//png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg'){
-    cb(null, true)
-  }else{
-    cb(null, false)
+  if (
+    file.mimetype === "image//png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
   }
-}
+};
 
 // app.use(express.urlencoded()) // x-www-form-urlencoded <form>
 app.use(express.json()); //application/json
-//Registering the multer with the storage and filefilter setted 
-app.use(multer({storage:fileStorage, fileFilter: fileFilter}).single('image'))
+//Registering the multer with the storage and filefilter setted
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+);
 //Creating a static folder to serve all the requests from images
-app.use('/images', express.static(path.join(__dirname, 'images')))
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 //Setting especial headers to avoid CORS error
 app.use((req, res, next) => {
@@ -48,35 +55,51 @@ app.use((req, res, next) => {
     "GET, POST, PUT, PATCH, DELETE"
   );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  //Set a response
+  if(req.method === 'OPTIONS'){
+    return res.sendStatus(200)
+  }
   next();
 });
 
-//Using and fowarding any incoming request of auth
-app.use("/auth", authRoutes);
-//Using and fowarding any incoming request of feed
-app.use("/feed", feedRoutes);
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: graphqlSchema,
+    rootValue: graphqlResolver,
+    //Enable the IDE
+    graphiql: true,
+    //Formatting the error
+    customFormatErrorFn(err){
+      //If it's not an original error, if it is a technical error
+      if(!err.originalError){
+        return err
+      }
+      //Retrieving from resolver the characteristic of error
+      const data = err.originalError.data
+      const message = err.originalError.message || 'An error occurred!'
+      const code = err.originalError.code || 500
+      return {message: message, status: code, data: data}
+    }
+
+  })
+);
 
 //Error middleware this always has to stay at last middleware
 app.use((error, req, res, next) => {
   //If status code is undefined, then the status will be 500
-  const status = error.statusCode || 500
+  const status = error.statusCode || 500;
   //Extracting the error message
-  const message = error.message
-  const data = error.data
+  const message = error.message;
+  const data = error.data;
   //Responding to front the status and message
-  res.status(status).json({message:message, data: data})
-})
+  res.status(status).json({ message: message, data: data });
+});
 
 //Connecting to mongoose and creating a messages db
-mongoose.connect(
-  "mongodb+srv://conrado:262800@cluster0.gpslw.mongodb.net/messages"
-).then(result => {
-    const server = app.listen(8080);
-    //Setting the socket connection using the http protocol
-    const io = require('./socket').init(server)
-    //Creating and event listener to each connection done
-    io.on('connection', socket => {
-      console.log('Client connected')
-    })
-}).catch(err => console.log(err));
-
+mongoose
+  .connect("mongodb+srv://conrado:262800@cluster0.gpslw.mongodb.net/messages")
+  .then((result) => {
+    app.listen(8080);
+  })
+  .catch((err) => console.log(err));
---
+++ graphql/resolvers.js
@@ -0,0 +1,41 @@
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+const validator = require("validator");
+
+module.exports = {
+  //Creating a function exactly the same as schema
+  //Getting the argument passed by schema
+  createUser: async function ({ userInput }, req) {
+    const errors = [];
+    if (!validator.isEmail(userInput.email)) {
+      errors.push({ message: "E-mail is invalid!" });
+    }
+    if (
+      validator.isEmpty(
+        userInput.password ||
+          !validator.isLength(userInput.password, { min: 4 })
+      )
+    ) {
+      errors.push({ message: "Password too short" });
+    }
+    if (errors.length > 0) {
+      const error = new Error("Invalid Input");
+      error.data = errors
+      error.code = 422
+      throw error;
+    }
+    const existingUser = await User.findOne({ email: userInput.email });
+    if (existingUser) {
+      const error = new Error("User already exists!");
+      throw error;
+    }
+    const hashedPw = await bcrypt.hash(userInput.password, 12);
+    const user = new User({
+      email: userInput.email,
+      name: userInput.name,
+      password: hashedPw,
+    });
+    const createdUser = await user.save();
+    return { ...createdUser._doc, _id: createdUser._id.toString() };
+  },
+};
